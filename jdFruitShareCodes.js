10579039aa453e06b52efe1e8453d42e1c21b64b
liguozong <741372449@qq.com>
2021-05-30 13:33:59 +0800

    update share code

--- jdFruitShareCodes.js
+++ jdFruitShareCodes.js
@@ -8,24 +8,25 @@
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let FruitShareCodes = [
-  '0a74407df5df4fa99672a037eec61f7e@dbb21614667246fabcfd9685b6f448f3@6fbd26cc27ac44d6a7fed34092453f77@61ff5c624949454aa88561f2cd721bf6',//账号一的好友shareCode,不同好友中间用@符号隔开
-  '6fbd26cc27ac44d6a7fed34092453f77@61ff5c624949454aa88561f2cd721bf6@9c52670d52ad4e1a812f894563c746ea@8175509d82504e96828afc8b1bbb9cb3',//账号二的好友shareCode，不同好友中间用@符号隔开
-]
-// 判断github action里面是否有水果互助码
+        '531d64c2d3aa40c4a558c9becf60743d@526cfa4c25bb488cbbe3c7f91865a0dc', //账号一的好友shareCode,不同好友中间用@符号隔开
+        '526cfa4c25bb488cbbe3c7f91865a0dc@d4a97ae6962941f7a264dbe9e7bfeecc', //账号二的好友shareCode，不同好友中间用@符号隔开
+        'd4a97ae6962941f7a264dbe9e7bfeecc@531d64c2d3aa40c4a558c9becf60743d'
+    ]
+    // 判断github action里面是否有水果互助码
 if (process.env.FRUITSHARECODES) {
-  if (process.env.FRUITSHARECODES.indexOf('&') > -1) {
-    console.log(`您的东东农场互助码选择的是用&隔开\n`)
-    FruitShareCodes = process.env.FRUITSHARECODES.split('&');
-  } else if (process.env.FRUITSHARECODES.indexOf('\n') > -1) {
-    console.log(`您的东东农场互助码选择的是用换行隔开\n`)
-    FruitShareCodes = process.env.FRUITSHARECODES.split('\n');
-  } else {
-    FruitShareCodes = process.env.FRUITSHARECODES.split();
-  }
+    if (process.env.FRUITSHARECODES.indexOf('&') > -1) {
+        console.log(`您的东东农场互助码选择的是用&隔开\n`)
+        FruitShareCodes = process.env.FRUITSHARECODES.split('&');
+    } else if (process.env.FRUITSHARECODES.indexOf('\n') > -1) {
+        console.log(`您的东东农场互助码选择的是用换行隔开\n`)
+        FruitShareCodes = process.env.FRUITSHARECODES.split('\n');
+    } else {
+        FruitShareCodes = process.env.FRUITSHARECODES.split();
+    }
 } else if (process.env.JD_COOKIE) {
-  console.log(`由于您secret里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
+    console.log(`由于您secret里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
 }
 for (let i = 0; i < FruitShareCodes.length; i++) {
-  const index = (i + 1 === 1) ? '' : (i + 1);
-  exports['FruitShareCode' + index] = FruitShareCodes[i];
+    const index = (i + 1 === 1) ? '' : (i + 1);
+    exports['FruitShareCode' + index] = FruitShareCodes[i];
 }
